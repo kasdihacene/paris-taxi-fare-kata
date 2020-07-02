ed0f75b359018918cc4a2f242c1f14fdf28e880c
Hacene <hacene.upmc@gmail.com>
2020-07-02 11:57:22 +0200

    Unit test for Main Class

---
+++ src/main/java/org/taxi/paris/api/TaxiApplication.java
@@ -0,0 +1,11 @@
+package org.taxi.paris.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiApplication.class, args);
+    }
+}
---
+++ src/test/java/org/taxi/paris/api/ApplicationTest.java
@@ -0,0 +1,11 @@
+package org.taxi.paris.api;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+    @Test
+    public void mainApplicationShouldLoadAllBeansTest(){
+        TaxiApplication.main(new String[]{});
+    }
+}
