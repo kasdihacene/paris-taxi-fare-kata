946fe62b725d7ec595f8495194f0e271a0d45e0d
Hacene <hacene.upmc@gmail.com>
2020-07-02 16:37:29 +0200

    TDD => Riding 4 miles on busy period (start Time >= 16:00 and start Time < 20:00 )

--- src/main/java/org/taxi/paris/api/services/TaxiService.java
+++ src/main/java/org/taxi/paris/api/services/TaxiService.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Service;
 import org.taxi.paris.api.domain.Ride;
 import org.taxi.paris.api.usecases.TaxiUseCase;
 
+import java.time.*;
+
 @Service
 public class TaxiService implements TaxiUseCase {
     /**
@@ -19,6 +21,16 @@ public class TaxiService implements TaxiUseCase {
 
     @Override
     public Double priceOf(Ride ride) {
+        String startTime = ride.getStartTime();
+        Instant instant = Instant.parse(startTime);
+        LocalDateTime dateTime = LocalDateTime.ofInstant(instant, ZoneId.of(ZoneOffset.UTC.getId()));
+
+        if ((dateTime.toLocalTime().isAfter(LocalTime.of(16, 0)) || dateTime.toLocalTime().equals(LocalTime.of(16, 0)))
+                && dateTime.toLocalTime().isBefore(LocalTime.of(20, 0))) {
+            Double price = 1 + 1 + (ride.getDistance() * 5) * 0.5;
+            return price;
+        }
+
         Double price = 1 + (ride.getDistance() * 5) * 0.5;
         return price;
     }
--- src/test/java/org/taxi/paris/api/services/TaxiServiceTest.java
+++ src/test/java/org/taxi/paris/api/services/TaxiServiceTest.java
@@ -1,6 +1,7 @@
 package org.taxi.paris.api.services;
 
 import org.assertj.core.api.Assertions;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.taxi.paris.api.domain.Ride;
 import org.taxi.paris.api.usecases.TaxiUseCase;
@@ -26,6 +27,48 @@ public class TaxiServiceTest {
         // ARRANGE
         Double expectedPrice = 8.5;
         Ride ride = new Ride(1, 3, "2020-06-19T13:01:17.031Z", 9000);
+        TaxiUseCase taxiUseCase = new TaxiService();
+
+        // ACT
+        Double actualPrice = taxiUseCase.priceOf(ride);
+
+        // ASSERT
+        Assertions.assertThat(actualPrice).isEqualTo(expectedPrice);
+    }
+
+    @Test
+    public void shouldReturnPriceRelatedToBusyPeriodWhenRidingAt4PM(){
+        // ARRANGE
+        Double expectedPrice = 12.0;
+        Ride ride = new Ride(1, 4, "2020-06-19T16:01:17.031Z", 9000);
+        TaxiUseCase taxiUseCase = new TaxiService();
+
+        // ACT
+        Double actualPrice = taxiUseCase.priceOf(ride);
+
+        // ASSERT
+        Assertions.assertThat(actualPrice).isEqualTo(expectedPrice);
+    }
+
+    @Test
+    public void shouldReturnPriceRelatedToBusyPeriodWhenRidingAt7PM(){
+        // ARRANGE
+        Double expectedPrice = 12.0;
+        Ride ride = new Ride(1, 4, "2020-06-19T19:01:17.031Z", 9000);
+        TaxiUseCase taxiUseCase = new TaxiService();
+
+        // ACT
+        Double actualPrice = taxiUseCase.priceOf(ride);
+
+        // ASSERT
+        Assertions.assertThat(actualPrice).isEqualTo(expectedPrice);
+    }
+
+    @Test
+    public void shouldReturnPriceRelatedToBusyPeriodWhenRidingAt4AM(){
+        // ARRANGE
+        Double expectedPrice = 12.0;
+        Ride ride = new Ride(1, 4, "2020-06-19T16:00:00.000Z", 9000);
         TaxiUseCase taxiUseCase = new TaxiService();
 
         // ACT
