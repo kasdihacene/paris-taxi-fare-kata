673aa9eca72d36176301bfb4e7740bf5ea1e749b
Hacene <hacene.upmc@gmail.com>
2020-07-02 11:51:55 +0200

    initialize env

---
+++ src/test/java/org/taxi/paris/api/InitTest.java
@@ -0,0 +1,12 @@
+package org.taxi.paris.api;
+
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+
+public class InitTest {
+
+    @Test
+    public void initializeEnv() {
+        Assertions.assertThat(true).isTrue();
+    }
+}
