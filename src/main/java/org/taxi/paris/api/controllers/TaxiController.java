a62c6d29e64f129eb2f31e5518a4be0d134f4061
Hacene <hacene.upmc@gmail.com>
2020-07-05 12:02:49 +0200

    add cross origin

--- src/main/java/org/taxi/paris/api/controllers/TaxiController.java
+++ src/main/java/org/taxi/paris/api/controllers/TaxiController.java
@@ -15,7 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/ride")
-@CrossOrigin(origins = "http://localhost:3000/")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaxiController {
 
     private TaxiUseCase taxiUseCase;
