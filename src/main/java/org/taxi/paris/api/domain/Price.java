a68894addc024ef667f8bc0d9ab2b2dd3d003d0d
Hacene <hacene.upmc@gmail.com>
2020-07-05 12:25:12 +0200

    add codecov plugin

--- src/main/java/org/taxi/paris/api/domain/Price.java
+++ src/main/java/org/taxi/paris/api/domain/Price.java
@@ -2,10 +2,8 @@ package org.taxi.paris.api.domain;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
-import lombok.ToString;
 
 @Getter
-@ToString
 @EqualsAndHashCode
 public class Price {
     private double price;
